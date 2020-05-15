8541b8e6e93c9d5bf1d921b1fb42a19b09ce291d
gisandan0321 <gisandanraniego@gmail.com>
2020-05-15 23:46:36 +0800

    Update Login and Registration Page

--- lib/Login.dart
+++ lib/Login.dart
@@ -14,9 +14,18 @@ class _LoginState extends State<Login> {
 
   Widget build(BuildContext context) {
     return new ListView(
+      padding: EdgeInsets.fromLTRB(20, 50, 20, 20),
       children: <Widget>[
-        new Center(
-          child: new Text("Just add any widget below or replace this centered text widget"),
+        new Text("Login Page",
+          textAlign: TextAlign.center,
+        ),
+        new SizedBox(height: 20),
+        new Text("Just add any widget below or replace this centered text widget",
+          textAlign: TextAlign.center,
+        ),
+        new SizedBox(height: 20),
+        new Text("Press the back button to go back of your phone to the previous screen",
+          textAlign: TextAlign.center,
         )
       ],
     );
